ccc306dc3534b5c97fc15db4672fdb07f0d40ba4
Felipe Machado <adoteototo@gmail.com>
2022-05-12 19:19:57 -0300

    3-crinado o primeiro endpoint

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const UserRoutes = require("./routes/UserRoutes");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(cors({
-  credentials:true,origin:'http://localhost:3000'
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
-app.use(express.static('public'))
+//routes
+app.use("/users", UserRoutes);
+
+app.use(express.static("public"));
 //
-app.listen(5000)
+app.listen(5000);

a3b1eef365a15755e8c74a3f4ab7148c904b2663
Felipe Machado <adoteototo@gmail.com>
2022-05-17 16:43:31 -0300

    8-salvando o token no header do postman e resgatando o mesmo no backend,no entanto o dado resgatado não foi trabalhado ainda

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.get("/checkuser", UserController.checkUser);
 
 module.exports = router;

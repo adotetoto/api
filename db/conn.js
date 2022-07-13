cbce04f93c38840ea2090ecc73ad5ffa8c85e4bc
adote o toto <adoteototo@gmail.com>
2022-07-13 11:41:41 -0300

    att

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,16 @@
-const  mongoose  = require("mongoose");
+const mongoose = require("mongoose");
+/*
+  ? MONGODB ATLAS
+ "mongodb+srv://adoteototo:33119525@cluster0.kndjb64.mongodb.net/?retryWrites=true&w=majority"
 
-async function main(){
-  await mongoose.connect("mongodb://localhost:27017/adoteototo")
+ ? MONGODB LOCAL
+ "mongodb://localhost:27017/adoteototo"
+ */
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/adoteototo");
   console.log("Conectou ao mongoose!");
 }
 
-main().catch((err)=>console.log(err))
+main().catch((err) => console.log(err));
 
-module.exports = mongoose
+module.exports = mongoose;
